5f96e0e70e3a7f3e54c6a85ab3d900106c4b2c58
a <ruan65@gmail.com>
2018-07-07 00:59:05 +0300

    return fab

--- app/src/main/java/org/premiumapp/antime/TimerActivity.kt
+++ app/src/main/java/org/premiumapp/antime/TimerActivity.kt
@@ -1,9 +1,11 @@
 package org.premiumapp.antime
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+
 import kotlinx.android.synthetic.main.activity_timer.*
 
 class TimerActivity : AppCompatActivity() {
@@ -12,6 +14,11 @@ class TimerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
         setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
